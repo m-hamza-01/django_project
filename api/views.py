240002c56d3f4bdd3f8e42cb02b799a5543704ae
Jhanzeb Shah <119981249+m-hamza-01@users.noreply.github.com>
2025-11-10 18:20:03 +0500

    new changes

--- api/views.py
+++ api/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, OrderItem
 
 # Create your views here.
 
 def say_hello(request):
 
-    queryset = Product.objects.values('id','title','collection__title').order_by('title')[0:10]
+    # queryset = Product.objects.values('id','title','collection__title').order_by('title')[0:10]
+    # queryset = Product.objects.filter(unit_price__range=(100, 200))
 
+    # queryset = Product.objects.filter(id__in=OrderItem.objects.values('product__id').distinct()).order_by('title')
+    # queryset = OrderItem.objects.values('product__id').distinct()
 
+    queryset = Product.objects.prefetch_related('promotions').all()
     
     return render(request, 'hello.html',{'name':'Hamza','products':list(queryset)})
